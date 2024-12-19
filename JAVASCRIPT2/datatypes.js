d67783a6a1f85c3b50c74c1f78f175aff063b578
yash <yashjadaun2711@gmail.com>
2024-12-19 23:39:51 +0530

    commit

---
+++ JAVASCRIPT2/datatypes.js
@@ -0,0 +1,29 @@
+//number
+var num = 2;
+console.log(num);
+
+
+//string
+var name = "Yash";
+console.log(name);
+
+//boolean
+var age = 18;
+var canVote = age>18;
+
+console.log(canVote);
+
+
+//bigint
+
+var num = 123455666677n;
+console.log(num);
+
+
+//Null
+// var box = null;
+// console.log(box);
+
+//undefind
+// var box;
+// console.log(box); // Output: undefined
---
+++ JAVASCRIPT2/loops.js
@@ -0,0 +1,64 @@
+//for loops
+// let i=1;
+
+// while(i<=10000){
+//     console.log(i);
+//     i++;
+// }
+
+
+//for loops
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+
+//do while loops
+// let num = 1;
+
+// do{
+//     console.log(num);
+//     num++;
+  
+// }while(num<=10);
+
+
+//multiplication table
+
+// let num=1;
+
+// while(num<=10){
+//     console.log("5*" + num + "=" + 5*num);
+//     num++;
+    
+// }
+
+
+// for(;;) {
+//     console.log("Running");
+
+// }
+
+
+
+//sum of number from 1 to 10
+
+// var sum = 0;
+
+// for (var i = 0; i <= 10; i++) {
+//     sum += i; // Adds the current value of `i` to `sum`
+    
+// console.log(sum);// Logs the updated `sum` to the console
+// }
+
+
+
+let year =2002;
+
+if(year%4==0 && year%100!=0){
+    console.log("It is a leap year");
+}
+else{
+    console.log("Not a leap year");
+}
+
